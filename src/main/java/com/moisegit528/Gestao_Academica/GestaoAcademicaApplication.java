a109277fe255eb651790857a7dd4a74421ac693a
Moises Castro <moisesgithub@gmail.com>
2026-08-04 20:56:34 -0300

    Initial commit

---
+++ src/main/java/com/moisegit528/Gestao_Academica/GestaoAcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.moisegit528.Gestao_Academica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAcademicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moisegit528/Gestao_Academica/GestaoAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisegit528.Gestao_Academica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
